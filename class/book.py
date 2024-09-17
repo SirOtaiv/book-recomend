e8f87640eba0fc09d73eef5a23f80a767505dcaa
SirOtaiv <rauotavio7@gmail.com>
2024-09-17 20:53:41 -0300

    Primeiras classes de referência

---
+++ class/book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, titulo: str, autor: int, genero: str, ano: int):
+        self.titulo = titulo
+        self.autor = autor
+        self.genero = genero
+        self.ano = ano
+
---
+++ class/user.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, nome: str, generoPref: str, autorPref: str, anoPref: int) -> None:
+        self.nome = nome
+        self.generoPref = generoPref
+        self.autorPref = autorPref
+        self.anoPref = anoPref
