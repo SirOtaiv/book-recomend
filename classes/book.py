2a7b4f1bde5c30e473c99f45da43cb8f3af1a0b2
SirOtaiv <rauotavio7@gmail.com>
2024-09-24 19:04:04 -0300

    Limpado repositório

--- classes/book.py
+++
@@ -1,7 +0,0 @@
-class Book:
-    def __init__(self, titulo: str, autor: int, genero: str, ano: int):
-        self.titulo = titulo
-        self.autor = autor
-        self.genero = genero
-        self.ano = ano
-
