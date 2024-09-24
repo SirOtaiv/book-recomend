9a6e758d17d3015aea7c466d0a1a70addb93fcad
SirOtaiv <rauotavio7@gmail.com>
2024-09-24 18:24:37 -0300

    Formatado output para melhor leitura do código

--- first_users.py
+++ first_users.py
@@ -28,8 +28,11 @@ def recomendarLivros(pageSize: int, userRec: User):
     lista_paginada_livros = lista_livros.sort_values(by=['tier','nome'], ascending=[False, True])
 
     print(f"Livros recomendados para {userRec.nome}:")
+    print(f"{'Livro':<40} - {'Autor':<30} - {'Gênero':<15} - {'Ano':<5} - {'Tier'}")
+    print("=" * 100)  # Linha de separação
     for _, row in lista_paginada_livros.head(pageSize).iterrows():
-        print(f"{row['nome']} - {row['autor']} - {row['ano_de_publicacao']} - {row['tier']}")
+        ano_publicacao = int(row['ano_de_publicacao']) if not pd.isna(row['ano_de_publicacao']) else 'N/A'
+        print(f"{row['nome']:<40} - {row['autor']:<30} - {row['genero']:<15} - {ano_publicacao:<5} - {row['tier']}")
 
 def criar_usuario():
     nome = input("Digite seu nome: ")
